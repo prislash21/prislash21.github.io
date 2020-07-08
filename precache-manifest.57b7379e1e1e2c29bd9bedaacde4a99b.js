2a154a46df763211fd0a9070ae4ef686442e76a8
prislash21 <prislash21@gmail.com>
2020-07-08 23:44:42 +0600

    UPDATED

---
+++ precache-manifest.57b7379e1e1e2c29bd9bedaacde4a99b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a52a53fb36e7db9be04ea436f34c14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "134deec75c3a74c1fde3",
+    "url": "/static/css/main.68b364d8.chunk.css"
+  },
+  {
+    "revision": "03746c32c0342e4737ab",
+    "url": "/static/js/2.e66f6289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e66f6289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "134deec75c3a74c1fde3",
+    "url": "/static/js/main.4b0b9e16.chunk.js"
+  },
+  {
+    "revision": "55f7ea1d5793e3fae0f6",
+    "url": "/static/js/runtime-main.a6bacbd1.js"
+  }
+]);
